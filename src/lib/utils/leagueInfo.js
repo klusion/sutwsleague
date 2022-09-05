54be8ac141d1fc76427f51c3904f0ebe6f9cd12a
klusion <klusion@gmail.com>
2022-09-05 02:31:21 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -80,7 +80,7 @@ export const managers = [
        "tookOver": 2016, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Nashville", // (optional)
        "bio": "I love fantasy football!",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/andrew.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
