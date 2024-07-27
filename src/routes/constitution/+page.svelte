cdb3e27186b5c94b42b5c13c4ee4af1c24636e31
klusion <klusion@gmail.com>
2024-07-27 18:50:23 -0500

    Update +page.svelte
    Updated the scoring and accidental player drops

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -173,6 +173,8 @@
 	
     <ul>
 	<li>Draft pick trading is limited to the period between the draft pick lottery and the end of the draft. The draft picks involved must be for the draft that year.</li>
+	<li>Several changes to scoring this year. See class="noUnderscore clickable" on:click={() => goToSection(four)}>"Section 4: Scoring" for details.</li>
+	<li></li>
     </ul>
     
     <!-- <p>18 Bench (Expanded by 5 between the keague draft and start of regular season)</p>
@@ -322,18 +324,18 @@
 	<li>Passing Touchdown 40+ yards = .5 bonus points</li>
     	<li>25 Completions = 1 bonus point</li>
     	<li>300-399 Passing Yards = 1 bonus point</li>
-    	<li>400+ Passing Yards = 1 bonus point</li>
+    	<li>400+ Passing Yards = 2 bonus points (Note: This bonus scoring replaces the previous bonus of 300-399 Passing Yards. The player will receive a total of 2 points not 3 points.)</li>
     </ul>
     
     <h3 bind:this={fourTwo}>4.2 - Rushing</h3>
     <ul>
         <li>10 Rushing Yards = 1 point</li>
-	<li>Carry = .1 point</li>
+	<li>Rushing 1st Down = .25 points (Note: This replaces our scoring last season for Rushing Attempts. Rushing attempts are no longer scored as .1 points.)</li>
         <li>Rushing Play of 40+ yards = .5 bonus points</li>
 	<li>Rushing Touchdown of 40+ yards = .5 bonus points</li>
-    	<li>20 Carries = 1 bonus point</li>
+    	<li>20 Carries = 2 bonus points</li>
     	<li>100-199 Rushing Yards = 1 bonus point</li>
-    	<li>200+ Rushing Yards = 1 bonus point</li>
+    	<li>200+ Rushing Yards = 2 bonus points (Note: This bonus scoring replaces the previous bonus of 100-199 Rushing Yards. The player will receive a total of 2 points not 3 points.)</li>
     	<li>200+ Rushing and Receiving Yards Combined = 1 bonus point</li>
     </ul>
 	
@@ -344,16 +346,15 @@
         <li>Receiving Play of 40+ yards = .5 bonus points</li>
 	<li>Receiving Touchdown of 40+ yards = .5 bonus points</li>
     	<li>100-199 Receiving Yards = 1 bonus point</li>
-    	<li>200+ Receiving Yards = 1 bonus point</li>
+    	<li>200+ Receiving Yards = 2 bonus points (Note: This bonus scoring replaces the previous bonus of 100-199 Receiving Yards. The player will receive a total of 2 points not 3 points.)</li>
     	<li>200+ Rushing and Receiving Yards Combined = 1 bonus point</li>
     </ul>
 
     <h3 bind:this={fourFour}>4.4 - Kicking</h3>
     <ul>
         <li>PAT = 1 point</li>
-	<li>FG Made (18-39 yards) = 3 points</li>
-        <li>FG Made (40-49 yards) = 4 points</li>
-	<li>FG Made (50+ yards) = 5 points</li>
+	<li>FG Made (18-49 yards) = 3 points (Note: 40-49 Yard FG Made is no longer scored as 4 points.)</li>
+        <li>FG Made (50+ yards) = 5 points</li>
     	<li>FG Miss = -1 point</li>
     </ul>
 
@@ -376,6 +377,9 @@
 	<li>Points Allowed (21-27) = -1 point</li>
 	<li>Points Allowed (28-34) = -2 points</li>
     	<li>Points Allowed (35+) = -3 points</li>
+	<li>300-399 Total Yards Allowed = -1 point</li>
+	<li>400-499 Total Yards Allowed = -2 points</li>
+    	<li>500+ Total Yards Allowed = -3 points</li>
     </ul>
 
     <!-- <ol>
@@ -411,6 +415,11 @@
 			<li>Friday = Waivers run at 6 a.m. central</li>
 			<li>Saturday = Waivers run at 6 a.m. central</li>
 		    </ul>
+    		</div>
+
+	<h4 bind:this={fiveOneTwo}>5.1.3 - Help! I accidentally dropped the wrong player!</h4>
+		<div class="subBlock">
+		<p>There is not much the commissioner can do if waivers have already run. Your best bet to to place a bid on the player you mistakenly dropped. However, if this occurred during the free agency period (Sunday or Monday when you do not have to submit a waiver claim), immediately contact the commissioner. If the player is still available on waivers, the commissioner may choose to communicate the mistake to the league and reverse the transaction.</p>
     		</div>
 
     <h3 bind:this={fiveTwo}>5.2 - Trades</h3>
